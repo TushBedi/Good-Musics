17cd5795a4237e0841ee7d1c3247cfd79684ff3e
mario <panggihmario@gmail.com>
2018-07-04 15:25:04 +0700

    versi 1.00 playlist

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
 let route = require('./routes/index.js')
-let user - require('./routes/users.js')
+let playlist = require('./routes/playlist.js')
 app.use(route)
-app.use(user)
+// app.use(user)
+
+
+app.use("/",playlist)
+
+
+
 app.listen(3000, () => console.log('Example app listening on port:V 3000!'))
